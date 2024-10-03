33ce3fbd617c8ea7c8c50ef73a38e394eeb7182b
Albinjohnz <86572237+Albinjohnz@users.noreply.github.com>
2024-10-04 00:09:05 +0100

    euler m

--- euler.js
+++ euler.js
@@ -2,12 +2,19 @@ alert("Load euler.js");
 a=3;
 b=5;
 num=1000;
+c=0;
+d=0;
 sum=0;
 for(i=1;i<num;i++)
 {
-    if((i%a==0)||(i%b==0))
+    if(i%a==0)
     {
-        sum=sum+0;
+        c=c+i;
+    }
+    if(i%b==0)
+    {
+        d=d+i;
     }
 }
+sum=c+d;
 alert(sum);
