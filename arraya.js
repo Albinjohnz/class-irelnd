0e0f518e2b7366e918fd873725856ac315d26a7b
Albinjohnz <86572237+Albinjohnz@users.noreply.github.com>
2024-10-04 14:51:49 +0100

    modified last

--- arraya.js
+++ arraya.js
@@ -1,14 +1,12 @@
 alert("arraya.js");
-function calculateArraySum(a,b,list) {
-    let a = [2];
-    let b=[3];
-    let list = [1, 2, 3, 4, 6, 7, 8, 9]; 
-    let total=0;
-    list.forEach((Item) => {
-        if(item%a==0  || item%b==0){
-            total +=Item;
+
+function calculateArraySum(a, b, list) {
+    let total = 0;
+    list.forEach((item) => {
+        if (item % a === 0 || item % b === 0) {
+            total += item;
         }
     });
-    alert(total); 
+    alert(total);
 }
-
+calculateArraySum(2, 3, [1, 2, 3, 4, 6, 7, 8, 9]);
