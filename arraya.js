f1ff5e1e0a31e576e64d45400a7ebd10ee9a6368
Albinjohnz <86572237+Albinjohnz@users.noreply.github.com>
2024-10-04 14:23:06 +0100

    modified code

--- arraya.js
+++ arraya.js
@@ -1,13 +1,17 @@
-alert("arraya.js")
+alert("arraya.js");
+
 function sumOfMultiples(a, l) {
     return l.reduce((sum, x) => {
         return sum + (a.some(num => x % num === 0) ? x : 0);
     }, 0);
 }
-function array(a,l)
-{
-const a=[1,2]
-const l=[1,2,3,4,6,7,8,9]
-const sum1=sumOfMultiples(a,l)
+
+function calculateArraySum() {
+    const multiples = [1, 2]; // Array of multiples
+    const list = [1, 2, 3, 4, 6, 7, 8, 9]; // List of integers
+    const sum1 = sumOfMultiples(multiples, list); // Calculate the sum
+    alert(sum1); 
 }
-alert(sum1)
+
+
+calculateArraySum();
